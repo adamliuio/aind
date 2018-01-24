c6e8a0403201398dac77b3cbf205c8f51390fb37
Adam Liu <adamkingleo@gmail.com>
2018-01-25 05:56:08 +1100

    .

--- t.py
+++ t.py
@@ -1,1 +1,7 @@
-from subprocess import call
+
+base_folder = "base_folder/"
+bf_net = "something"
+
+step_5_weights_path = base_folder + 'saved_models/weights.best.step_5.{}.hdf5'.format(bf_net)
+
+print(step_5_weights_path)
--- t.sh
+++
@@ -1,5 +0,0 @@
-wget https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/dogImages.zip
-unzip dogImages.zip
-wget https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/lfw.zip
-unzip lfw.zip
-wget https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/DogVGG16Data.npz
